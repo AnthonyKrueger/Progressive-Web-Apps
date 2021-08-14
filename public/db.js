c8d123fcde6a764cbfdbfc00dc6b2f5d3df1f7de
aik <anthonykrueger0@gmail.com>
2021-08-14 11:35:11 -0500

    indexDb updates on reconnect

--- public/db.js
+++ public/db.js
@@ -22,3 +22,37 @@ request.onsuccess =({target}) => {
         checkDatabase()
     }
 }
+
+request.onerror = function(event) {
+    console.log("something went wrong")
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], 'readwrite')
+    const store = transaction.createObjectStore("pending")
+
+    store.add(record)
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending", "readwrite"])
+    const store = transaction.createObjectStore("pending")
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => {
+                return response.json()
+            })
+        }
+    }
+}
